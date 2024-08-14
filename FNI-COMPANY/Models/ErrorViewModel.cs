b86f8abc7ce033d2bea566794c135ed27be3a3e1
santhosh <santhoshcellamuthuc@gmail.com>
2024-08-14 18:21:25 +0530

    Add project files.

---
+++ FNI-COMPANY/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FNI_COMPANY.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
